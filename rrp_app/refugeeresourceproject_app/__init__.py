c480826456791d55a63e0d4fa75c1e280f1eb969
Jack-R-Long <jack.richard.long@gmail.com>
2022-06-24 23:02:00 -0500

    Delete everything, Django normal start

--- rrp_app/refugeeresourceproject_app/__init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request, redirect, session
-app = Flask(__name__)
-app.secret_key = "ALL 4 one, 1 for all."
--- rrp_app/server.py
+++
@@ -1,5 +0,0 @@
-from refugeeresourceproject_app import app
-from refugeeresourceproject_app.controllers import controllers_users
-
-if __name__ == '__main__':
-    app.run(debug=True)
